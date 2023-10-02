fbe7929ef92d6ba70dc6e6afe42865ca348f3f3c
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-02 13:27:54 +0200

    fdg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   // const title = 'In the shadow of the shield';
@@ -9,6 +11,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         {/* <h1>{title}</h1>
         <p>Liked {likes} times</p>
@@ -18,7 +21,7 @@ function App() {
         <p>{Math.ceil(Math.random() * 10)}</p>
 
         <a href={link} target='blank'>Google site</a> */}
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
