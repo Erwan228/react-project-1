6d4a8145d8a9b270016158e05649fa8a0a58e2ba
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-05 14:39:34 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,34 @@
+import { useState } from "react";
+
 const Home = () => {
-
+    // let name = 'Thancred';
+    const [name, setName] = useState('Thancred'); //reactive variable with variableName, then setVariableName. Data type in useState can be whatevs
+    const [age, setAge] = useState(32)
     const handleClick = (e) => {
-        console.log('damage points', e);
+        setName('Urianger');
+        setAge(29)
+        // console.log('damage points', e);
+    };
+    const handleClick2 = (e) => {
+        setName('Krile');
+        setAge(22)
+        // console.log('damage points', e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name)
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target)
+    // }
 
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => {
-                handleClickAgain('Music')
-            }}>Click me again</button>
+            <button onClick={handleClick2}>Click me</button>
+            {/* <button onClick={(e) => {
+                handleClickAgain('Music', e)
+            }}>Click me again</button> */}
         </div>
     );
 }
