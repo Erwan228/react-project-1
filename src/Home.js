04c33758d743ec3d41ceb6ec3ff6b58516605548
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-16 13:25:29 +0200

    nh

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,40 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     // let name = 'Thancred';
-    const [name, setName] = useState('Thancred'); //reactive variable with variableName, then setVariableName. Data type in useState can be whatevs
-    const [age, setAge] = useState(32)
-    const handleClick = (e) => {
-        setName('Urianger');
-        setAge(29)
-        // console.log('damage points', e);
-    };
-    const handleClick2 = (e) => {
-        setName('Krile');
-        setAge(22)
-        // console.log('damage points', e);
-    }
+    // const [name, setName] = useState('Thancred'); //reactive variable with variableName, then setVariableName. Data type in useState can be whatevs
+    // const [age, setAge] = useState(32)
+    // const handleClick = (e) => {
+    //     setName('Urianger');
+    //     setAge(29)
+    // console.log('damage points', e);
+    // };
+    // const handleClick2 = (e) => {
+    //     setName('Krile');
+    //     setAge(22)
+    // console.log('damage points', e);
+    // }
 
     // const handleClickAgain = (name, e) => {
     //     console.log('hello ' + name, e.target)
     // }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Music', id: 1 },
+        { title: 'Welcome pary!', body: 'lorem ipsum...', author: 'Keith', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Melinda', id: 3 },
+    ])
 
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} old</p>
+            {/* <p>{name} is {age} old</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={handleClick2}>Click me</button>
+            <button onClick={handleClick2}>Click me</button> */}
             {/* <button onClick={(e) => {
                 handleClickAgain('Music', e)
             }}>Click me again</button> */}
+            <BlogList blogs={blogs} title='All blogs!' />
         </div>
     );
 }
