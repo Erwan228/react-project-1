a65ca9999d88c98e44baf7f9c018401cc24f6e16
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-23 14:13:51 +0200

    manhandle error

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 
 const Home = () => {
     // let name = 'Thancred';
@@ -19,36 +21,13 @@ const Home = () => {
     // const handleClickAgain = (name, e) => {
     //     console.log('hello ' + name, e.target)
     // }
-    const [blogs, setBlogs] = useState(null)
-
     const [name, setName] = useState('Music')
-
-    const [isPending, setIsPending] = useState(true);
-
-    const [error, setError] = useState(null);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fetch the data for that resource, pls fix')
-                    }
-                    return res.json()
-                })
-                .then(data => {
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false)
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-                })
-        }, 1000);
-    }, []);
+
+
+
 
 
     return (
