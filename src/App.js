8132ac802acc7b63962c97b62458e3ebe705b28f
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-24 12:58:13 +0200

    df

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Navbar from './Navbar'
-import Home from './Home'
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   // const title = 'In the shadow of the shield';
@@ -9,20 +10,27 @@ function App() {
 
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        {/* <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{'Hello, Paladins'}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.ceil(Math.random() * 10)}</p>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* <h1>{title}</h1>
+          <p>Liked {likes} times</p>
+          <p>{10}</p>
+          <p>{'Hello, Paladins'}</p>
+          <p>{[1, 2, 3, 4, 5]}</p>
+          <p>{Math.ceil(Math.random() * 10)}</p>
 
-        <a href={link} target='blank'>Google site</a> */}
-        <Home />
+          <a href={link} target='blank'>Google site</a> */}
+          {/* <Home /> */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
