b4d0393e3bd39d9fe88fd50438347b7ba49e6a34
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-26 14:40:24 +0200

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -7,10 +12,14 @@ const Create = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 />
                 <label>Blog author:</label>
                 <select>
@@ -18,6 +27,8 @@ const Create = () => {
                     <option value="Melinda">Melinda</option>
                 </select>
                 <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
             </form>
         </div>
     );
