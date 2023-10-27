e0ca40f1944693d64247455f5d2af89810675a74
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-10-27 10:58:38 +0200

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -3,11 +3,19 @@ import { useState } from "react";
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Music');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog)
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
@@ -22,13 +30,17 @@ const Create = () => {
                     onChange={(e) => setBody(e.target.value)}
                 />
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="Music">Music</option>
                     <option value="Melinda">Melinda</option>
                 </select>
                 <button>Add blog</button>
                 <p>{title}</p>
                 <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
